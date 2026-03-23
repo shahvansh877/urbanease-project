492ddb7608d4f1bd5122822e42edea4ead3ac6c6
Pragadeeshwar Vijay <dvpppragadeeshwar@gmail.com>
2026-03-24 02:27:58 +0530

    login issue solved

--- server/server.js
+++ server/server.js
@@ -11,6 +11,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5173",
+    "https://urbanease-final.vercel.app/",
     process.env.CLIENT_URL
   ],
   credentials: true,
